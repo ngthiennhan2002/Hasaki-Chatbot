479621eb630c89c0d6e49bdd6eeb040b8dd88baa
Thien Nhan <ngthiennhan2002@gmail.com>
2025-01-03 23:40:24 +0700

    Update text2sql.py

--- Source code/text2sql.py
+++ Source code/text2sql.py
@@ -28,39 +28,5 @@ def execution(user_query, df_list, df_names):
     # Run execution
     user_query += str(tables)
     user_query += "Chỉ trả về kết quả mã SQL, không trả về bất cứ thông tin nào khác."
-    messages = [
-        {"role": "user", "content": user_query},
-    ]
-    from transformers import AutoModelForCausalLM, AutoTokenizer
-
-    model_name = "Qwen/Qwen2.5-Coder-32B-Instruct"
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        torch_dtype="auto",
-        device_map="auto"
-    )
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    messages = [
-        {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
-        {"role": "user", "content": user_query}
-    ]
-    text = tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True
-    )
-    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
-
-    generated_ids = model.generate(
-        **model_inputs,
-        max_new_tokens=512
-    )
-    generated_ids = [
-        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-    ]
-
-    response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
     
     return response
